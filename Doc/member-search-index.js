f148ca07a2302251407f96cde07f4b1fb0ca2d62
AntoMei <90893283+AntoMei@users.noreply.github.com>
2022-02-28 17:04:52 +0100

    Documentación del código.

---
+++ Doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.company","c":"GeneradorDePrimos","l":"GeneradorDePrimos()","u":"%3Cinit%3E()"},{"p":"com.company","c":"GeneradorDePrimos","l":"generarPrimos(int)"},{"p":"com.company","c":"GeneradorDePrimos","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
